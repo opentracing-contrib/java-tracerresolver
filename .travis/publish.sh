cf669c48b8357f34dc9dd859b0cc1adcdee179fb
Sjoerd Talsma <sjoerdtalsma@users.noreply.github.com>
2017-04-28 13:05:09 +0200

    Change travis script (0.0.1 didn't publish javadoc+sources to bintray) (#11)

--- .travis/publish.sh
+++ .travis/publish.sh
@@ -112,13 +112,13 @@ fi
 
 ./mvnw verify -nsu
 
-# If we are on a pull request, our only job is to run tests, which happened above via ./mvnw install
+# If we are on a pull request, our only job is to run tests, which happened above via ./mvnw verify
 if is_pull_request; then
   true
 # If we are on master, we will deploy the latest snapshot or release version
 #   - If a release commit fails to deploy for a transient reason, delete the broken version from bintray and click rebuild
 elif is_travis_branch_master; then
-  ./mvnw --batch-mode -s .mvn/settings.xml -Prelease -nsu -DskipTests deploy
+  ./mvnw --batch-mode -s .mvn/settings.xml -Prelease -nsu -DskipTests package deploy
 
   # If the deployment succeeded, sync it to Maven Central. Note: this needs to be done once per project, not module, hence -N
   if is_release_commit; then
